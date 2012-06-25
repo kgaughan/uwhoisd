1a1a0c0db8a01c0b0b8362eafb30e024b1b24754
Keith Gaughan <k@stereochro.me>
2012-06-25 01:46:47 +0100

    setup.py et al.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='uwhoisd',
+    version='0.0.1',
+    description="Universal domain WHOIS proxy server.",
+    long_description=open('README').read(),
+    url='https://github.com/kgaughan/uwhoisd/',
+    install_requires=[line.rstrip() for line in open('requirements.txt')],
+    py_modules=['uwhoisd'],
+
+    entry_points={
+        'console_scripts': [
+            'uwhoisd = uwhoisd:main'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+    ],
+
+    author='Keith Gaughan',
+    author_email='k@stereochro.me',
+)
