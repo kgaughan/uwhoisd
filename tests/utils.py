41e0bc7f816c1fb7fc262f262ec24d7cfbf4ca17
Keith Gaughan <k@stereochro.me>
2012-08-30 22:44:31 +0100

    tests/__init__.py and some testing utility functions.

---
+++ tests/utils.py
@@ -0,0 +1,25 @@
+"""
+Utility functions for testing.
+"""
+
+from os import path
+
+import uwhoisd
+
+
+HERE = path.dirname(__file__)
+
+
+def create_uwhois():
+    """Prepare a UWhois object for testing."""
+    parser = uwhoisd.make_default_config_parser()
+    parser.read(path.join(HERE, '..', 'uwhois.ini'))
+    uwhois = uwhoisd.UWhois()
+    uwhois.read_config(parser)
+    return uwhois
+
+
+def read_transcript(name):
+    """Read a WHOIS transcript file."""
+    with open(path.join(HERE, 'transcripts', name), 'r') as fh:
+        return fh.read()
