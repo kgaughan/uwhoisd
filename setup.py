a6f2e3bd35b5a1dc8a479dcf23f818bfb09dfbdd
Keith Gaughan <k@stereochro.me>
2012-09-01 23:34:04 +0100

    Record metadata in one spot only; read requirements smartly.

--- setup.py
+++ setup.py
@@ -1,15 +1,49 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import re
+
+
+def read(filename):
+    with open(filename, 'r') as fh:
+        return fh.read()
+
+
+def get_metadata(module_path):
+    """Extract the metadata from a module file."""
+    matches = re.finditer(
+        r"^__(\w+?)__ *= *'(.*?)'$",
+        read(module_path),
+        re.MULTILINE)
+    return dict(
+        (match.group(1), match.group(2).decode('unicode_escape'))
+        for match in matches)
+
+
+def read_requirements(requirements_path):
+    """Read a requirements file, stripping out the detritus."""
+    requirements = []
+    with open(requirements_path, 'r') as fh:
+        for line in fh:
+            line = line.strip()
+            if line != '' and not line.startswith(('#', 'svn+', 'git+')):
+                requirements.append(line)
+    return requirements
+
+
+META = get_metadata('uwhoisd.py')
+
 
 setup(
     name='uwhoisd',
-    version='0.0.4',
+    version=META['version'],
     description="Universal domain WHOIS proxy server.",
-    long_description=open('README').read(),
+    long_description=read('README'),
     url='https://github.com/kgaughan/uwhoisd/',
-    install_requires=[line.rstrip() for line in open('requirements.txt')],
+    license='MIT',
+    install_requires=read_requirements('requirements.txt'),
     py_modules=['uwhoisd'],
+    zip_safe=True,
 
     entry_points={
         'console_scripts': [
@@ -28,6 +62,6 @@ setup(
         'Topic :: System :: Networking',
     ],
 
-    author='Keith Gaughan',
-    author_email='k@stereochro.me',
+    author=META['author'],
+    author_email=META['email']
 )
--- uwhoisd.py
+++ uwhoisd.py
@@ -1,42 +1,18 @@
-#
-# uwhoisd
-# by Keith Gaughan <http://stereochro.me/>
-#
-# Copyright (c) Keith Gaughan, 2012
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-
 """
 A 'universal' WHOIS proxy server.
 """
 
-import diesel
-import re
-import os.path
-import sys
-import socket
-import time
+from ConfigParser import SafeConfigParser
 import collections
 import functools
 import logging
-from ConfigParser import SafeConfigParser
+import os.path
+import re
+import socket
+import sys
+import time
+
+import diesel
 
 
 __version__ = '0.0.4'
