5a65034f7b8c01f54c9abb2e87cd8b5bbf9513b7
Keith Gaughan <k@stereochro.me>
2024-08-24 13:24:46 +0100

    Further cleanup

--- src/uwhoisd/caching.py
+++ src/uwhoisd/caching.py
@@ -24,7 +24,8 @@ def get_cache(cfg):
     if cache_name == "null":
         logger.info("Caching deactivated")
         return None
-    for ep in metadata.entry_points(name="uwhoisd.cache"):
+    eps = metadata.entry_points()
+    for ep in eps.get("uwhoisd.cache", []):
         if ep.name == cache_name:
             logger.info("Using cache '%s' with the parameters %r", cache_name, cfg)
             cache_type = ep.load()
