1968c564420420d116fa049fe56cf7734cf133c5
Keith Gaughan <k@stereochro.me>
2024-10-21 02:34:00 +0100

    Await the read and close the connection at the end

--- src/uwhoisd/server.py
+++ src/uwhoisd/server.py
@@ -5,7 +5,7 @@ from . import utils
 
 async def start_service(iface: str, port: int, whois):
     async def handle_request(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        query = reader.read_until(b"\r\n")
+        query = await reader.readuntil(b"\r\n")
         cleaned = query.decode().strip().lower()
         if not utils.is_well_formed_fqdn(cleaned):
             result = f"; Bad query: '{cleaned}'\r\n"
@@ -13,6 +13,7 @@ async def start_service(iface: str, port: int, whois):
             result = await whois(cleaned)
         writer.write(result.encode())
         await writer.drain()
+        writer.close()
 
     svr = await asyncio.start_server(handle_request, host=iface, port=port)
     async with svr:
